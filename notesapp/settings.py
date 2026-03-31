7285e65e8a852e8e8bd361be46091e93d8a710aa
Som Shankar Pandey <90376051+somingit55@users.noreply.github.com>
2026-04-01 00:16:22 +0530

    Update settings.py

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -87,24 +87,24 @@ WSGI_APPLICATION = 'notesapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
-    }
-}
-#DATABASE CONFIGURATION (sqlite3 for Docker)
 #DATABASES = {
+
     #'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / "db.sqlite3",
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME': os.getenv("DB_NAME"),
+        #'USER': os.getenv("DB_USER"),
+        #'PASSWORD': os.getenv("DB_PASSWORD"),
+        #'HOST': os.getenv("DB_HOST"),
+        #'PORT': os.getenv("DB_PORT"),
     #}
 #}
+DATABASE CONFIGURATION (sqlite3 for Docker)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 
